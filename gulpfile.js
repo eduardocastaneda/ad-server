2b5fc9a86c257448392e095ec7fd8ee1486a6c2a
eduardocastaneda <eduardo.castaneda@valtech.se>
2016-08-05 13:04:18 +0200

    small gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
-    server = require('gulp-express')
+    server = require('gulp-express');
 
 gulp.task('server', function () { 
     server.run(['app.js'],[], false);
-})
+});
 
 gulp.task('watch', function (){
   gulp.watch(['app.js', 'controllers/**/*.js'], [server.run]);
-})
+});
 
-gulp.task('default', ['server','watch'])
+gulp.task('default', ['server','watch']);
