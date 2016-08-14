eed6b6d0ed9230dd9da5695aeec96983989bcfd0
eduardocastaneda <eduardo.castaneda@valtech.se>
2016-08-14 13:48:30 +0200

    added tests

---
+++ test/functions.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var functions = require('../lib/functions');
+
+describe('reverse function', function () {
+  it('Should reverse words in message', function () {
+    var message = 'Idag åt jag en macka till frukost'
+    var reversedMessage = functions.reverseMessage(message);
+
+    assert.equal(reversedMessage, 'gadi tå gaj ne akcam llit tsokurf');
+  });
+});
+
+describe('get most reversed list', function () {
+  it('Should get eight of the most reversed messages', function () {
+    var messages = functions.getMostReversed();
+
+    assert.strictEqual(messages.length, 8);
+  });
+});
